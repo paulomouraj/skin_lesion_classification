5ffac1ccc6db24ad18f150f32841689397d26908
paulomouraj <167315301+paulomouraj@users.noreply.github.com>
2024-09-11 22:24:22 +0200

    Updated docs

--- README.md
+++ README.md
@@ -1,40 +1,49 @@
 # Classification of Skin Lesions
 
 ## Description
-The goal of this work was to perform supervised classification of skin lesions to 8 diagnostic classes. This project is part of [Télécom Paris Image Processing](https://www.telecom-paris.fr/fr/ingenieur/formation/2e-annee-orientation/image) course and its goal was to apply classical machine learning methods and convolutional neural networks (CNN) to classify medical images in a highly imbalanced dataset. 
-  
+
+The goal of this work was to perform supervised classification of skin lesions to 8 diagnostic classes. This project is part of [Télécom Paris Image Processing](https://www.telecom-paris.fr/fr/ingenieur/formation/2e-annee-orientation/image) course and its goal was to apply classical machine learning methods and convolutional neural networks (CNN) to classify medical images in a highly imbalanced dataset. In order to apply the classical machine learning methods, a segmentation pipeline was implemented based on UNet CNN architecture.
+
 Classical machine learning methods used
-* K-Nearest Neighbours
-* Suppport Vectors Classifier with BRF kernel
-* Previous methods with oversampling (ADASYN)
-  
-Convolutional neural networks used:
-* LeNet-5
-* Inception-ResNet-v2
+
+- K-Nearest Neighbours
+- Suppport Vectors Classifier with BRF kernel
+- Previous methods with oversampling (ADASYN)
+
+Convolutional neural networks used for classification and segmentation:
+
+- LeNet-5
+- Inception-ResNet-v2
+- UNet
 
 ## Dataset
+
 The skin lesion images along with metadata were taken from ISIC dataset, available on https://challenge.isic-archive.com/data/.
 
 ## Requirements
+
 ### For classical machine learning methods
-* Python 3.10
-* NumPy 1.26.0
-* Pandas 2.2.1
-* Scikit-Image 0.21.0
-* Scikit-Learn 1.4.2
-* OpenCV 4.8.1
-* Joblib 0.14.0
-* Imbalanced-learn 0.12.3
-  
+
+- Python 3.10
+- NumPy 1.26.0
+- Pandas 2.2.1
+- Scikit-Image 0.21.0
+- Scikit-Learn 1.4.2
+- OpenCV 4.8.1
+- Joblib 0.14.0
+- Imbalanced-learn 0.12.3
+
 ### Additionals for deep learning methods
-* PyTorch 2.3.0
-* PyTorch-cuda 11.8
-* Pillow 10.4.0
-* TorchVision 0.18.0
-* PyTorch Image Models (timm) 0.1.8 : https://huggingface.co/timm
-* Tqdm 2.2.3
+
+- PyTorch 2.3.0
+- PyTorch-cuda 11.8
+- Pillow 10.4.0
+- TorchVision 0.18.0
+- PyTorch Image Models (timm) 0.1.8 : https://huggingface.co/timm
+- Tqdm 2.2.3
 
 ## Contributors
+
 Paulo Roberto de Moura Júnior (me)
 
 [High level description]()
--- pipelines/resnet_training.py
+++ pipelines/resnet_training.py
@@ -9,6 +9,13 @@ import torchvision.transforms as transforms
 from tqdm import tqdm
 import timm
 
+############################################################################################################
+## Pipeline to train a ResNet model on the skin lesion dataset, created to be run in a Python script.     ##
+## The model is trained on the training set and the best model is saved.                                  ##
+## The training metrics are saved to a file.                                                             ##
+############################################################################################################
+
+
 class CustomDataset(Dataset):
     def __init__(self, root, metadata, train, num_channels, transform=None):
         self.metadata = metadata
@@ -37,7 +44,8 @@ class CustomDataset(Dataset):
             return image, label
 
         return image
-    
+
+
 metadata_path = 'metadata/metadataTrain.csv'
 
 metadata_train = pd.read_csv(metadata_path)
@@ -49,37 +57,46 @@ metadata_file = 'metadata/metadataTest.csv'
 img_transform = transforms.Compose([
     transforms.Resize((299, 299)),
     transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                         std=[0.229, 0.224, 0.225]),
 ])
 
 # Load the dataset
-train_data = CustomDataset(train_dir, metadata_train, train=True, num_channels=3, transform=img_transform)
+train_data = CustomDataset(train_dir, metadata_train,
+                           train=True, num_channels=3, transform=img_transform)
 
 # separate into train and validation
 train_size = int(0.8 * len(train_data))
 val_size = len(train_data) - train_size
-train_dataset, val_dataset = torch.utils.data.random_split(train_data, [train_size, val_size], generator=torch.Generator().manual_seed(0))
+train_dataset, val_dataset = torch.utils.data.random_split(
+    train_data, [train_size, val_size], generator=torch.Generator().manual_seed(0))
 
 # check GPU to work
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 torch.cuda.empty_cache()
 print("Device: ", device)
 
-model = timm.create_model('inception_resnet_v2', pretrained=False, num_classes=8)
+model = timm.create_model('inception_resnet_v2',
+                          pretrained=False, num_classes=8)
 
 learning_rate = 0.0001
 num_epochs = 20
 batch_size = 32
 
-train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, generator=torch.Generator().manual_seed(0), num_workers=os.cpu_count())
-val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=os.cpu_count())
+train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True,
+                          generator=torch.Generator().manual_seed(0), num_workers=os.cpu_count())
+val_loader = DataLoader(val_dataset, batch_size=batch_size,
+                        shuffle=False, num_workers=os.cpu_count())
 
 # train model
 model.to(device)
-class_weights = [0.7005531, 0.24592265, 0.95261733, 3.64804147, 1.20674543, 13.19375, 12.56547619, 5.04219745]
-criterion = nn.CrossEntropyLoss(weight=torch.tensor(class_weights, dtype=torch.float32).to(device))
+class_weights = [0.7005531, 0.24592265, 0.95261733,
+                 3.64804147, 1.20674543, 13.19375, 12.56547619, 5.04219745]
+criterion = nn.CrossEntropyLoss(weight=torch.tensor(
+    class_weights, dtype=torch.float32).to(device))
 optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=2)
+scheduler = optim.lr_scheduler.ReduceLROnPlateau(
+    optimizer, mode='min', factor=0.1, patience=2)
 
 # Initialize lists to store training and validation metrics
 train_losses = []
@@ -112,8 +129,9 @@ for epoch in range(num_epochs):
             total_train += labels.size(0)
             correct_train += (predicted == labels).sum().item()
 
-            train_tqdm.set_postfix({'train_loss': train_loss.item(), 'train_accuracy': 100 * correct_train / total_train})
-    
+            train_tqdm.set_postfix({'train_loss': train_loss.item(
+            ), 'train_accuracy': 100 * correct_train / total_train})
+
     # Calculate average training loss and accuracy for the epoch
     avg_train_loss = epoch_train_loss / len(train_loader)
     avg_train_accuracy = 100 * correct_train / total_train
@@ -139,20 +157,20 @@ for epoch in range(num_epochs):
             _, predicted = torch.max(val_outputs, 1)
             total_val += val_labels.size(0)
             correct_val += (predicted == val_labels).sum().item()
-            
+
     # Calculate average validation loss and accuracy for the epoch
     avg_val_loss = epoch_val_loss / len(val_loader)
     avg_val_accuracy = 100 * correct_val / total_val
     val_losses.append(avg_val_loss)
     val_accuracies.append(avg_val_accuracy)
-    
+
     # Update the scheduler according to validation loss
     scheduler.step(avg_val_loss)
 
     # Display training and validation loss, accuracy for the epoch
     current_lr = optimizer.param_groups[0]['lr']  # Get current learning rate
     print(f'Epoch [{epoch + 1}/{num_epochs}], Train Loss: {avg_train_loss:.4f}, Train Accuracy: {avg_train_accuracy:.2f}%, Val Loss: {avg_val_loss:.4f}, Val Accuracy: {avg_val_accuracy:.2f}%, LR: {current_lr}')
-    
+
     # Check if current validation accuracy is better than the best so far
     if avg_val_accuracy > best_val_accuracy:
         best_val_accuracy = avg_val_accuracy
@@ -164,6 +182,7 @@ file_path = "resnet_metrics1.txt"
 with open(file_path, "w") as file:
     file.write("Epoch\tTrain Loss\tVal Loss\tTrain Accuracy\tVal Accuracy\n")
     for epoch in range(num_epochs):
-        file.write(f"{epoch+1}\t{train_losses[epoch]}\t{val_losses[epoch]}\t{train_accuracies[epoch]}\t{val_accuracies[epoch]}\n")
+        file.write(
+            f"{epoch+1}\t{train_losses[epoch]}\t{val_losses[epoch]}\t{train_accuracies[epoch]}\t{val_accuracies[epoch]}\n")
 
 print("Training metrics saved to:", file_path)
